8386cd8cbe819c330b42ef349c43274d046ba6b8
Damon <chaoming5677@gmail.com>
2019-12-25 22:27:22 +0800

    Create promise_chain.js

---
+++ promise_chain.js
@@ -0,0 +1,19 @@
+const methodThatReturnsAPromise = id =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(id)
+      resolve(id)
+    }, 2000)
+
+    if (id == 'fuck') {
+      console.log('promise fuck up')
+      reject('my fuck')
+    }
+  })
+
+
+[1, 2, 3, 'fuck', 5].reduce((previousPromise, nextID) => {
+  return previousPromise.then(() => {
+    return methodThatReturnsAPromise(nextID)
+  })
+}, Promise.resolve())
