d48b216bc60d51ddcddcd4632e9613e465eabc25
chaoming56 <chaoming5677@gmail.com>
2022-09-28 17:02:51 +0800

    flaten dfs

--- algorithm/flatten.js
+++ algorithm/flatten.js
@@ -3,37 +3,29 @@ var input = {
   b: [1, 2, { c: true }, [311, [232323232]]],
   d: { e: 22, f: 33 },
   g: null,
-  h: { i: { j: 'fff' } },
-};
-
-function flatten(data) {
-  let res = {};
-
-  function helper(values, key = '') {
-    if (!values) return;
-
-    Object.keys(values).forEach((k) => {
-      const value = values[k];
-
-      let __key;
-      if (Array.isArray(values)) {
-        __key = key ? `${key}[${k}]` : k;
-      } else {
-        __key = key ? `${key}.${k}` : k;
-      }
-      
-      if (typeof value === 'object') {
-        helper(value, __key)
-      } else {
-        res[__key] = value
-      }
-    });
-  }
-
-  helper(data);
-  return res;
+  h: { i: { j: 'done' } },
 }
 
-const res = flatten(input);
+function dfs(data) {
+  const res = {}
 
-console.log(res)
+  function helper(obj, preKey = '') {
+    const isObjArray = Array.isArray(obj)
+    Object.keys(obj).forEach((k) => {
+      const key = isObjArray
+        ? `${preKey}[${k}]`
+        : `${!preKey ? '' : preKey + '.'}${k}`
+      if (obj[k] instanceof Array) {
+        helper(obj[k], key)
+      } else {
+        res[key] = obj[k]
+      }
+    })
+  }
+
+  helper(data)
+  return res
+}
+
+const res1 = dfs(input)
+console.log(res1)
